5aa046984ead86fc8ad8d2c007d73c79dda9a226
Frederich Munch <marsupial@users.noreply.github.com>
2016-06-06 22:01:53 -0400

    CLING: Live lookup of selectors from Objective-C runtime (OS X only). Enabled via CMake CLING_OBJC_SUPPORT=ON

--- lib/CodeGen/CGObjCMac.cpp
+++ lib/CodeGen/CGObjCMac.cpp
@@ -43,6 +43,18 @@
 using namespace clang;
 using namespace CodeGen;
 
+#ifdef CLING_OBJC_SUPPORT
+
+#include "clang/CodeGen/IObjCLookup.h"
+
+namespace cling {
+  namespace objectivec {
+    IObjCLookup* gInstance;
+  }
+}
+
+#endif
+
 namespace {
 
 // FIXME: We should find a nicer way to make the labels for metadata, string
@@ -1053,6 +1065,22 @@ protected:
   /// EmitImageInfo - Emit the image info marker used to encode some module
   /// level information.
   void EmitImageInfo();
+
+  llvm::Constant *
+  getSelectorConstant(const Selector &Sel,
+                      const ObjCCommonTypesHelper &ObjCTypes) {
+#ifdef CLING_OBJC_SUPPORT
+    using namespace cling::objectivec;
+    if (gInstance) {
+      return llvm::Constant::getIntegerValue(
+          ObjCTypes.SelectorPtrTy,
+          llvm::APInt(sizeof(uintptr_t) * 8, uintptr_t(gInstance->getSelector(
+                                                 Sel.getAsString().c_str()))));
+    }
+#endif
+    return llvm::ConstantExpr::getBitCast(GetMethodVarName(Sel),
+                                          ObjCTypes.SelectorPtrTy);
+  }
 
 public:
   CGObjCCommonMac(CodeGen::CodeGenModule &cgm) :
@@ -4973,9 +5001,7 @@ Address CGObjCMac::EmitSelectorAddr(CodeGenFunction &CGF, Selector Sel) {
 
   llvm::GlobalVariable *&Entry = SelectorReferences[Sel];
   if (!Entry) {
-    llvm::Constant *Casted =
-      llvm::ConstantExpr::getBitCast(GetMethodVarName(Sel),
-                                     ObjCTypes.SelectorPtrTy);
+    llvm::Constant *Casted = getSelectorConstant(Sel, ObjCTypes);
     Entry = CreateMetadataVar(
         "OBJC_SELECTOR_REFERENCES_", Casted,
         "__OBJC,__message_refs,literal_pointers,no_dead_strip", Align, true);
@@ -7315,9 +7341,7 @@ Address CGObjCNonFragileABIMac::EmitSelectorAddr(CodeGenFunction &CGF,
 
   CharUnits Align = CGF.getPointerAlign();
   if (!Entry) {
-    llvm::Constant *Casted =
-      llvm::ConstantExpr::getBitCast(GetMethodVarName(Sel),
-                                     ObjCTypes.SelectorPtrTy);
+    llvm::Constant *Casted = getSelectorConstant(Sel, ObjCTypes);
     Entry = new llvm::GlobalVariable(CGM.getModule(), ObjCTypes.SelectorPtrTy,
                                      false, llvm::GlobalValue::PrivateLinkage,
                                      Casted, "OBJC_SELECTOR_REFERENCES_");
