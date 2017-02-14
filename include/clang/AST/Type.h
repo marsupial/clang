ffc9fa58714f3653dcb5cab45697b25997be637b
Frederich Munch <marsupial@users.noreply.github.com>
2017-02-13 21:57:49 -0500

    CLING: Add ASTContext::forgetDecls method.

--- include/clang/AST/Type.h
+++ include/clang/AST/Type.h
@@ -3687,6 +3687,7 @@ class TagType : public Type {
   TagDecl * decl;
 
   friend class ASTReader;
+  friend class ASTContext;  // CLING
 
 protected:
   TagType(TypeClass TC, const TagDecl *D, QualType can);
