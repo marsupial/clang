874cbcd8ac7893cc8f5c8c45e1f7c03a282cbd31
Frederich Munch <marsupial@users.noreply.github.com>
2017-02-13 21:57:49 -0500

    CLING: Add ASTContext::forgetDecls method.

--- include/clang/AST/ASTContext.h
+++ include/clang/AST/ASTContext.h
@@ -2603,6 +2603,8 @@ public:
   };
 
   llvm::StringMap<SectionInfo> SectionInfos;
+
+  void forgetDecls(std::set<Decl*>& Decls); // CLING
 };
 
 /// \brief Utility function for constructing a nullary selector.
