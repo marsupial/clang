0e2596b18909815c738596127711cad4fe59b97a
Frederich Munch <marsupial@users.noreply.github.com>
2017-02-13 22:01:00 -0500

    CLING: Make DeclUnloader a friend to TagDecl to reach IsCompleteDefinition.

--- include/clang/AST/Decl.h
+++ include/clang/AST/Decl.h
@@ -3050,6 +3050,7 @@ public:
 
   friend class ASTDeclReader;
   friend class ASTDeclWriter;
+  friend class cling::DeclUnloader;
 };
 
 /// EnumDecl - Represents an enum.  In C++11, enums can be forward-declared
